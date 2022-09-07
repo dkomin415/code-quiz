8e420965a86fcf0de8136470de5ad376ca91ee21
dkomin415 <dkomin415.sales@gmail.com>
2022-09-06 22:05:37 -0700

    password hashing

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../../config/connection');
+const { use } = require('../routes');
 
 // create our User model
 class User extends Model {}
@@ -32,6 +34,17 @@ User.init(
     }
   },
   {
+    hooks: {
+      async beforeCreate(userData) {
+        newUserData.password = await bcrypt.hash(userData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
